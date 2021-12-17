5e1ca2b7b085ac92b9267cb0f6ff5df7daedd3fa
brorica <minjea917@gmail.com>
2021-12-17 18:12:20 +0900

    Revert "Shp parser (#20)"
    This reverts commit de5a57e1ef27e8f53e64f67526a155666055a92b.

--- src/main/java/shp/ShpMain.java
+++ src/main/java/shp/ShpMain.java
@@ -4,17 +4,11 @@ import static config.ApplicationProperties.getProperty;
 
 public class ShpMain {
 
-    public void run() {
-        try {
-            Shp shp = new Shp(getProperty("shp.fileName"));
-            ShpRepository shpRepository = new ShpRepository(shp);
-            shpRepository.save();
-//            ShpConvert shpConvert = new ShpConvert(shp);
-//            shpConvert.printAttributes(0, 0);
-//            TestDraw testDraw = new TestDraw(shp.getFile());
-//            testDraw.displayShapefile();
-        } catch(Exception e) {
-
-        }
+    public void run() throws Exception {
+        Shp shp = new Shp(getProperty("shp.fileName"));
+//        ShpConvert shpConvert = new ShpConvert(shp);
+//        shpConvert.printAttributes(0, 0);
+        TestDraw testDraw = new TestDraw(shp.getFile());
+        testDraw.displayShapefile();
     }
 }
